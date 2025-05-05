f5044411396d8754f19f8de75ef89c716c7bca1a
Parker <pgchandik@gmail.com>
2025-05-04 20:20:03 -0400

    many penduli

--- src/main.py
+++ src/main.py
@@ -12,12 +12,13 @@ from manim import (
     Dot,
     Line,
     linear,
+    TracedPath,
+    VMobject,
 )
-from networkx.algorithms.cycles import minimum_cycle_basis
 
 # rendering constants
-runtime = 10.0  # runtime of animation
-simulation_time = 20.0  # time experienced by simulation
+runtime = 20.0  # runtime of animation
+simulation_time = 10.0  # time experienced by simulation
 dt = simulation_time / (config.frame_rate * runtime)
 
 # physical constants
@@ -29,51 +30,141 @@ dtheta1_i, dtheta2_i = 0.0, 0.0
 
 #  #  #  #  #  #  #  #
 
-# simple pendulum example
-#
-# theta'' + (g / l) * sin(theta) = 0
-#   -> theta'' = - (g / l) * sin(theta)
 
+class DoublePendulum(VMobject):
+    def __init__(self, theta1_i, theta2_i, **kwargs):
+        VMobject.__init__(self, **kwargs)
 
-# 2nd order Euler
-# def update_pendulum(theta, dtheta, dt):
-#     a = - (g / l) * np.sin(theta[-1])
+        # values to keep track of
+        self.theta1 = ValueTracker(theta1_i)
+        self.hist_theta1 = [self.theta1.get_value()]
+        self.theta2 = ValueTracker(theta2_i)
+        self.hist_theta2 = [self.theta2.get_value()]
 
-#     theta_new = theta[-1] + dtheta[-1] * dt + 0.5 * a * dt**2
-#     dtheta_new = dtheta[-1] + a * dt
+        self.dtheta1 = ValueTracker(dtheta1_i)
+        self.hist_dtheta1 = [self.dtheta1.get_value()]
+        self.dtheta2 = ValueTracker(dtheta2_i)
+        self.hist_dtheta2 = [self.dtheta2.get_value()]
 
-#     return theta_new, dtheta_new
+        # draw mass and arm
+        self.mass1 = Dot()
+        self.add_updater(self.custom_update)
+        self.mass1.add_updater(
+            lambda mob: mob.move_to(
+                (l1 * np.sin(self.theta1.get_value())) * RIGHT
+                + (l1 * np.cos(self.theta1.get_value())) * DOWN
+            )
+        )
+
+        self.add(self.mass1)
+
+    #     self.mass2 = Dot()
+    #     self.mass2.add_updater(
+    #         lambda mob: mob.move_to(
+    #             self.mass1.get_center()
+    #             + (l2 * np.sin(self.theta2.get_value())) * RIGHT
+    #             + (l2 * np.cos(self.theta2.get_value())) * DOWN
+    #         )
+    #     )
+
+    #     self.add(self.mass2)
+
+    #     self.trace = TracedPath(self.mass2.get_start)
+    #     self.add(self.trace)
+
+    #     self.origin = Dot()
+
+    #     self.line1 = Line()
+    #     self.line1.add_updater(
+    #         lambda mob: mob.put_start_and_end_on(
+    #             self.origin.get_center(), self.mass1.get_center()
+    #         )
+    #     )
+    #     self.line2 = Line()
+    #     self.line2.add_updater(
+    #         lambda mob: mob.put_start_and_end_on(
+    #             self.mass1.get_center(), self.mass2.get_center()
+    #         )
+    #     )
+
+    #     self.add(self.line1, self.line2, self.origin)
+
+    # update function called once per frame
+    def custom_update(self, **kwargs):
+        print("hi")
+        # update_pendulum(
+
+    #     self.hist_theta1, self.hist_dtheta1, self.hist_theta2, self.hist_dtheta2
+    # )
+
+    # self.theta1.set_value(self.hist_theta1[-1])
+    # self.theta2.set_value(self.hist_theta2[-1])
+
+    # self.dtheta1.set_value(self.hist_dtheta1[-1])
+    # self.dtheta2.set_value(self.hist_dtheta2[-1])
 
 
 # acceleration for first pendulum
 # all inputs are lists with all historical values up to this point
-def f1(theta1, dtheta1, theta2, dtheta2):
-    return 0
+def diff(state):
+    theta1 = state[0]
+    dtheta1 = state[1]
+    theta2 = state[2]
+    dtheta2 = state[3]
+
+    delta_theta = theta1 - theta2
+    M = m1 + m2
+    alpha = m1 + m2 * (np.sin(delta_theta) ** 2)
+
+    eq6 = (
+        -m2 * l2 * (dtheta2**2) * np.sin(delta_theta)
+        - m2 * l1 * (dtheta1**2) * np.sin(delta_theta) * np.cos(delta_theta)
+        - M * g * np.sin(theta1)
+        + m2 * g * np.sin(theta2) * np.cos(delta_theta)
+    )
+    ddtheta1 = eq6 / (l1 * alpha)
+
+    eq7 = (
+        M * l1 * (dtheta1**2) * np.sin(delta_theta)
+        + m2 * l2 * (dtheta2**2) * np.sin(delta_theta) * np.cos(delta_theta)
+        + M * g * np.sin(theta1) * np.cos(delta_theta)
+        - M * g * np.sin(theta2)
+    )
+    # print("eq7: " + str(eq7))
+    # print("l2: " + str(l2))
+    # print("alpha: " + str(alpha))
+    ddtheta2 = eq7 / (l2 * alpha)
+
+    return np.array((dtheta1, ddtheta1, dtheta2, ddtheta2))
 
 
-# acceleration for second pendulum
-def f2(theta1, dtheta1, theta2, dtheta2):
-    return 0
-
-
-# bashforth adams
+# rk4 yass
 def update_pendulum(theta1, dtheta1, theta2, dtheta2):
-    theta1.append(theta1[-1] + 1.5 * dt * dtheta1[-1] - 0.5 * dt * dtheta2[-2])
-    dtheta1.append(
-        dtheta1[-1]
-        + 1.5 * dt * f1(theta1, dtheta1, theta2, dtheta2)
-        - 0.5 * dt * f1(theta1, dtheta1, theta2, dtheta2)
-    )
+    state = np.array((theta1[-1], dtheta1[-1], theta2[-1], dtheta2[-1]))
 
-    theta2.append(theta2[-1] + 1.5 * dt * dtheta2[-1] - 0.5 * dt * dtheta2[-2])
-    dtheta2.append(
-        dtheta2[-1]
-        + 1.5 * dt * f2(theta1, dtheta1, theta2, dtheta2)
-        - 0.5 * dt * f2(theta1, dtheta1, theta2, dtheta2)
-    )
+    k1 = diff(state)
+    k2 = diff(state + dt * (k1 / 2))
+    k3 = diff(state + dt * (k2 / 2))
+    k4 = diff(state + dt * k3)
+
+    # print("initial state:")
+    # print(state)
+
+    # print("diff")
+    # print(k1)
+
+    state += (dt / 6) * (k1 + 2 * k2 + 2 * +k3 + k4)
+
+    # print("final state:")
+    # print(state)
+
+    theta1.append(state[0])
+    dtheta1.append(state[1])
+    theta2.append(state[2])
+    dtheta2.append(state[3])
 
 
-class DoublePendulum(MovingCameraScene):
+class Simulation(MovingCameraScene):
     def construct(self):
         # print out some basic info about sim
         print("frame rate: " + str(config.frame_rate))
@@ -86,86 +177,90 @@ class DoublePendulum(MovingCameraScene):
 
         # time tracker needed for animation length
         time = ValueTracker(0)
-        hist_time = [time.get_value(), time.get_value()]
+        # hist_time = [time.get_value(), time.get_value()]
 
         # values to keep track of
-        theta1 = ValueTracker(theta1_i)
-        hist_theta1 = [theta1.get_value(), theta1.get_value()]
-        theta2 = ValueTracker(theta2_i)
-        hist_theta2 = [theta2.get_value(), theta2.get_value()]
+        # theta1 = ValueTracker(theta1_i)
+        # hist_theta1 = [theta1.get_value(), theta1.get_value()]
+        # theta2 = ValueTracker(theta2_i)
+        # hist_theta2 = [theta2.get_value(), theta2.get_value()]
 
-        dtheta1 = ValueTracker(dtheta1_i)
-        hist_dtheta1 = [dtheta1.get_value(), dtheta1.get_value()]
-        dtheta2 = ValueTracker(dtheta2_i)
-        hist_dtheta2 = [dtheta2.get_value(), dtheta2.get_value()]
+        # dtheta1 = ValueTracker(dtheta1_i)
+        # hist_dtheta1 = [dtheta1.get_value(), dtheta1.get_value()]
+        # dtheta2 = ValueTracker(dtheta2_i)
+        # hist_dtheta2 = [dtheta2.get_value(), dtheta2.get_value()]
 
-        print("time: " + str(hist_time[0]))
-        print("initial theta 1:  " + str(hist_theta1[0]))
-        print("initial dtheta 1: " + str(hist_dtheta1[0]))
-        print("initial theta 2:  " + str(hist_theta2[0]))
-        print("initial dtheta 2: " + str(hist_dtheta2[0]))
-        print()
+        # print("time: " + str(hist_time[0]))
+        # print("initial theta 1:  " + str(hist_theta1[0]))
+        # print("initial dtheta 1: " + str(hist_dtheta1[0]))
+        # print("initial theta 2:  " + str(hist_theta2[0]))
+        # print("initial dtheta 2: " + str(hist_dtheta2[0]))
+        # print()
 
         # update func to run per frame
-        def update(self):
-            hist_time.append(time.get_value())
-            # dt = hist_time[-1] - hist_time[-2]
+        # def update(self):
+        #     hist_time.append(time.get_value())
+        #     # dt = hist_time[-1] - hist_time[-2]
 
-            update_pendulum(hist_theta1, hist_dtheta1, hist_theta2, hist_theta2)
+        #     update_pendulum(hist_theta1, hist_dtheta1, hist_theta2, hist_dtheta2)
 
-            theta1.set_value(hist_theta1[-1])
-            theta2.set_value(hist_theta2[-1])
-            # hist_theta1.append(theta1_new)
+        #     theta1.set_value(hist_theta1[-1])
+        #     theta2.set_value(hist_theta2[-1])
+        #     # hist_theta1.append(theta1_new)
 
-            dtheta1.set_value(hist_dtheta1[-1])
-            dtheta2.set_value(hist_dtheta2[-1])
-            # hist_dtheta.append(dtheta_new)
+        #     dtheta1.set_value(hist_dtheta1[-1])
+        #     dtheta2.set_value(hist_dtheta2[-1])
+        #     # hist_dtheta.append(dtheta_new)
 
-        # draw mass and arm
-        mass1 = Dot()
-        mass1.add_updater(update)
-        mass1.add_updater(
-            lambda mob: mob.move_to(
-                (l1 * np.sin(theta1.get_value())) * RIGHT
-                + (l1 * np.cos(theta1.get_value())) * DOWN
-            )
-        )
+        # # draw mass and arm
+        # mass1 = Dot()
+        # mass1.add_updater(update)
+        # mass1.add_updater(
+        #     lambda mob: mob.move_to(
+        #         (l1 * np.sin(theta1.get_value())) * RIGHT
+        #         + (l1 * np.cos(theta1.get_value())) * DOWN
+        #     )
+        # )
 
-        self.add(mass1)
+        # self.add(mass1)
 
-        mass2 = Dot()
-        mass2.add_updater(
-            lambda mob: mob.move_to(
-                mass1.get_center()
-                + (l2 * np.sin(theta2.get_value())) * RIGHT
-                + (l2 * np.cos(theta2.get_value())) * DOWN
-            )
-        )
+        # mass2 = Dot()
+        # mass2.add_updater(
+        #     lambda mob: mob.move_to(
+        #         mass1.get_center()
+        #         + (l2 * np.sin(theta2.get_value())) * RIGHT
+        #         + (l2 * np.cos(theta2.get_value())) * DOWN
+        #     )
+        # )
 
-        self.add(mass2)
+        # self.add(mass2)
 
-        origin = Dot()
-        line1 = Line()
-        line1.add_updater(
-            lambda mob: mob.put_start_and_end_on(
-                origin.get_center(), mass1.get_center()
-            )
-        )
-        line2 = Line()
-        line2.add_updater(
-            lambda mob: mob.put_start_and_end_on(mass1.get_center(), mass2.get_center())
-        )
+        # trace = TracedPath(mass2.get_start)
+        # self.add(trace)
 
-        self.add(line1, line2, origin)
+        pend1 = DoublePendulum(theta1_i, theta2_i)
+        self.add(pend1)
+        # line1 = Line()
+        # line1.add_updater(
+        #     lambda mob: mob.put_start_and_end_on(
+        #         origin.get_center(), mass1.get_center()
+        #     )
+        # )
+        # line2 = Line()
+        # line2.add_updater(
+        #     lambda mob: mob.put_start_and_end_on(mass1.get_center(), mass2.get_center())
+        # )
+
+        # self.add(line1, line2, origin)
 
         self.play(
             time.animate.set_value(simulation_time), rate_func=linear, run_time=runtime
         )
 
         # print("yayay")
-        print(f"end time: {hist_time[-1]:.2f}")
-        print(f"final theta 1: {hist_theta1[-1]:.2f}")
-        print(f"final dtheta 1: {hist_dtheta1[-1]:.2f}")
-        print(f"final theta 2: {hist_theta2[-1]:.2f}")
-        print(f"final dtheta 2: {hist_dtheta2[-1]:.2f}")
-        print()
+        # print(f"end time: {hist_time[-1]:.2f}")
+        # print(f"final theta 1: {hist_theta1[-1]:.2f}")
+        # print(f"final dtheta 1: {hist_dtheta1[-1]:.2f}")
+        # print(f"final theta 2: {hist_theta2[-1]:.2f}")
+        # print(f"final dtheta 2: {hist_dtheta2[-1]:.2f}")
+        # print()
